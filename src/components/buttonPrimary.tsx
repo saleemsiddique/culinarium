f91d13f30725c17cbc3c48ca6fdff9f8c3428d30
Hakeem <126789780+HoopStar23@users.noreply.github.com>
2025-07-14 16:35:54 +0200

    manejar dirrecciones incorrectas

--- src/components/buttonPrimary.tsx
+++ src/components/buttonPrimary.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'; /* THIS IS A TEST TO NAVIGATE DELETE LATER */
 
 export default function ButtonPrimary() {
   return (
-    <Link href="/error404" className="bg-amber-400 text-black px-4 py-2 rounded shadow hover:bg-amber-600">
+    <Link href="/kitchen" className="bg-amber-400 text-black px-4 py-2 rounded shadow hover:bg-amber-600">
       Primary Action
     </Link >
   );
