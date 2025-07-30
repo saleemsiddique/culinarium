db88f009c4bb5973362d1e0ff67012d0fd1cc5a9
saleemsiddique <saleemprogramacion04@gmail.com>
2025-07-30 22:10:34 +0200

    vercel oficial

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 // app/api/openai/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import OpenAI from 'openai';
