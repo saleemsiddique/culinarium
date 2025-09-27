9aaa7765ca2c962ee2ed6d097b507cb40b3c70b8
saleemsiddique <saleemprogramacion04@gmail.com>
2025-09-27 15:19:56 +0200

    Update route.ts

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import { WelcomeEmailHtmlEN } from "@/app/emails/WelcomeEN"; //Welcome Email Ingles
