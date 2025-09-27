5359938d14d767e6a1ed56d8082a0636b4c28241
saleemsiddique <saleemprogramacion04@gmail.com>
2025-09-27 11:40:50 +0200

    vercel correcion

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-import { WelcomeEmailHtml } from "@/app/emails/Welcome";
-import { UnsubscribeEmailHtml } from "@/app/emails/UnsuscribedEmail";
+import { WelcomeEmailHtml } from "@/lib/emails/Welcome";
+import { UnsubscribeEmailHtml } from "@/lib/emails/UnsuscribedEmail";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
